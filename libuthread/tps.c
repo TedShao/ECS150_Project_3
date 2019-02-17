e9425bf89df6455efcc5a8cd64bb8a8fe55ba2d3
whgentry <whgentry@ucdavis.edu>
2019-02-17 13:33:58 -0800

    Phase 2.2 v1.0

--- libuthread/tps.c
+++ libuthread/tps.c
@@ -1,25 +1,26 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stddef.h>
 #include <assert.h>
 #include <pthread.h>
-#include <signal.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
-#include <unistd.h>
 
 #include "queue.h"
 #include "thread.h"
 #include "tps.h"
 
-/***** Structs *****/
+/***** Data Structures *****/
 typedef struct tps {
 	pthread_t tid;
 	char *ptr;
 } *tps_t;
 
 /***** Global Variables *****/
-queue_t tps_queue = NULL;
+static queue_t tps_queue = NULL;
 
 /***** Internal Functions *****/
 /* Enqueue item, if queue is null, create queue */
@@ -79,6 +80,42 @@ int find_tid(void *data, void *arg)
     return 0;
 }
 
+int find_ptr(void *data, void *arg)
+{
+    tps_t tps = (tps_t)data;
+
+    if ((void*)tps->ptr == arg) {
+        return 1;
+    }
+
+    return 0;
+}
+
+static void segv_handler(int sig, siginfo_t *si, void *context)
+{
+    /*
+     * Get the address corresponding to the beginning of the page where the
+     * fault occurred
+     */
+    void *p_fault = (void*)((uintptr_t)si->si_addr & ~(TPS_SIZE - 1));
+	void *void_ptr = NULL;
+
+    /*
+     * Iterate through all the TPS areas and find if p_fault matches one of them
+     */
+	queue_iterate(tps_queue, find_ptr, p_fault, &void_ptr);
+    if (void_ptr != NULL) {
+        /* Printf the following error message */
+        fprintf(stderr, "TPS protection error!\n");
+	}
+
+    /* In any case, restore the default signal handlers */
+    signal(SIGSEGV, SIG_DFL);
+    signal(SIGBUS, SIG_DFL);
+    /* And transmit the signal again in order to cause the program to crash */
+    raise(sig);
+}
+
 
 /***** API Definitions *****/
 int tps_init(int segv)
@@ -90,6 +127,15 @@ int tps_init(int segv)
 	}
 
 	initialized = 1;
+
+	if (segv) {
+		struct sigaction sa;
+		sigemptyset(&sa.sa_mask);
+		sa.sa_flags = SA_SIGINFO;
+		sa.sa_sigaction = segv_handler;
+		sigaction(SIGBUS, &sa, NULL);
+		sigaction(SIGSEGV, &sa, NULL);
+    }
 
 	return 0;
 }
@@ -112,7 +158,7 @@ int tps_create(void)
 	}
 
 	/* Allocate memory and check for proper allocation */
-	void_ptr = mmap(NULL, TPS_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
+	void_ptr = mmap(NULL, TPS_SIZE, PROT_NONE, MAP_PRIVATE | MAP_ANON, -1, 0);
 	if (void_ptr == MAP_FAILED) {
 		exit_critical_section();
 		return -1;
@@ -149,6 +195,7 @@ int tps_destroy(void)
 
 	/* Unmap memory and free struct data */
 	del_tps = (tps_t)void_ptr;
+	mprotect(del_tps->ptr, TPS_SIZE, PROT_READ | PROT_WRITE);
 	munmap(del_tps->ptr, TPS_SIZE);
 	free(del_tps);
 
@@ -194,7 +241,11 @@ int tps_read(size_t offset, size_t length, char *buffer)
 	}
 
 	access_tps = (tps_t) void_ptr;
+
+	/* Allow temporary read access */
+	mprotect(access_tps->ptr, TPS_SIZE, PROT_READ);
 	memcpy(buffer, (void*)(access_tps->ptr + offset), length);
+	mprotect(access_tps->ptr, TPS_SIZE, PROT_NONE);
 
 	exit_critical_section();
 	return 0;
@@ -235,7 +286,12 @@ int tps_write(size_t offset, size_t length, char *buffer)
 	}
 
 	access_tps = (tps_t) void_ptr;
+
+	/* Allow temporary write access */
+	mprotect(access_tps->ptr, TPS_SIZE, PROT_READ | PROT_WRITE);
 	memcpy((void*)(access_tps->ptr + offset), buffer, length);
+	mprotect(access_tps->ptr, TPS_SIZE, PROT_NONE);
+
 	exit_critical_section();
 	return 0;
 }
@@ -271,7 +327,7 @@ int tps_clone(pthread_t tid)
 	cpy_tps = (tps_t) void_ptr;
 
 	/* Allocate memory and check for proper allocation */
-	void_ptr = mmap(NULL, TPS_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
+	void_ptr = mmap(NULL, TPS_SIZE, PROT_NONE, MAP_PRIVATE | MAP_ANON, -1, 0);
 	if (void_ptr == MAP_FAILED) {
 		exit_critical_section();
 		return -1;
@@ -282,8 +338,16 @@ int tps_clone(pthread_t tid)
 	new_tps->tid = current_tid;
 	new_tps->ptr = (char*) void_ptr;
 
+	/* Unprotect memory */
+	mprotect(new_tps->ptr, TPS_SIZE, PROT_READ | PROT_WRITE);
+	mprotect(cpy_tps->ptr, TPS_SIZE, PROT_READ);
+
 	/* Copy memory */
 	memcpy((new_tps->ptr), (cpy_tps->ptr), TPS_SIZE);
+
+	/* Protect memory */
+	mprotect(cpy_tps->ptr, TPS_SIZE, PROT_NONE);
+	mprotect(new_tps->ptr, TPS_SIZE, PROT_NONE);
 
 	/* Enqueue the new tps */
 	tps_queue_enqueue_check(&tps_queue, (void*) new_tps);
